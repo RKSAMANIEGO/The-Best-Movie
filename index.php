152f38d2ab202315146e95c3da465e64f5506b55
rksam <erksg.10.26@gmail.com>
2025-03-04 11:43:27 -0500

    updat title page

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ function handlerBack()
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prueba</title>
+    <title>The Best Movie Sam</title>
     <!-- FontAwesome CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
